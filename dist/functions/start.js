afe46a0900cea81bde53e3b9c3394f84fc9caf25
Ramco <ramazane791@gmail.com>
2023-04-09 10:20:18 +0300

    Changes: clcn is updated

--- dist/functions/start.js
+++ dist/functions/start.js
@@ -1,31 +1,32 @@
 const { resolve } = require("path");
-const coloredConsole = require("clcn");
+const clcn = require("clcn");
 const { revoltHandler } = require("../revoltHandler");
 const { readdirSync } = require("fs");
-const clcn = new coloredConsole();
 
 function start(prop, handlerClient) {
   try {
     const commandsFolder = readdirSync(prop.path);
     for (const folder of commandsFolder) {
-      let commandFiles = readdirSync(`${prop.path}/${folder}`).filter((file) =>
-        file.endsWith(".js") || file.endsWith(".ts")
+      let commandFiles = readdirSync(`${prop.path}/${folder}`).filter(
+        (file) => file.endsWith(".js") || file.endsWith(".ts")
       );
       for (const file of commandFiles) {
         const command = require(resolve(`${prop.path}/${folder}/${file}`));
         if (!command?.default?.name || !command?.default?.code) {
-          clcn.log(
-            "txt:red",
-            `Failed Loaded command from ${resolve(
-              prop.path + "/" + folder + "/" + file
-            )} with revolthandler.js`
+          console.log(
+            clcn.txtRed(
+              `Failed Loaded command from ${resolve(
+                prop.path + "/" + folder + "/" + file
+              )} with revolthandler.js`
+            )
           );
         } else {
-          clcn.log(
-            "txt:blue",
-            `Waking up '${command?.default?.name}' from ${resolve(
-              prop.path + "/" + folder + "/" + file
-            )} with revolthandler.js`
+          console.log(
+            clcn.txtBlue(
+              `Waking up '${command?.default?.name}' from ${resolve(
+                prop.path + "/" + folder + "/" + file
+              )} with revolthandler.js`
+            )
           );
           handlerClient.commands.set(command.default.name, command);
         }
@@ -43,7 +44,7 @@ function start(prop, handlerClient) {
       const args = message.content.slice(prop.prefix.length).trim().split(/ +/);
       revoltHandler(message, args, prop.client, handlerClient, owners);
     });
-    clcn.log("txt:green", "revolthandler.js started");
+    console.log(clcn.txtGreen("revolthandler.js started"));
   } catch (e) {
     console.log(new Error(e.message));
   }
